ecdaab40e9fa0272844b95e6cb9556ff0ed0d5cb
Muhtadi <laskar90muhtadi@gmail.com>
2020-07-09 22:21:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const buttons = document.querySelectorAll(".audio-list div");
+
+    let arr = [];
+    arr.length = 0;
+    const tooglePlay = (src) => {
+        src.map(r => {
+            r.pause();
+            r.currentTime = 0;
+        });
+    };
+
+    buttons.forEach((clips, index) => {
+        clips.addEventListener("click", function(){
+            arr.push(sounds[index]);
+            tooglePlay(arr);
+           sounds[index].play(); 
+        });
+    });
+
+});
